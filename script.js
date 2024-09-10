b010e086c857c016be1aa7b86bfb9a4d8d0bec76
CerrajeriaSmartkey <cerrajeriastk@hotmail.com>
2024-09-10 12:02:47 -0600

    Subir pagina web
    se subio los archivos html, css de la pagina web

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        centeredSlides: true,
+        loop: true,
+        spaceBetween: 30,
+        grabCursor: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            991: {
+                slidesPerView: 1
+            }
+        }
+    });
+});
